41b113cd0b98d9dec446764bead012c2b0d2d7f2
Gian Libunao <gianlibunao@gmail.com>
2019-04-26 03:08:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(number)
+ if number<=1
+  false
+  elsif number==2
+  true
+  else
+  array=(2...number).to_a
+  flag = 0
+  array.each do |x|
+    rem=number%x
+    if rem==0
+      flag+=1
+    end
+  end
+  if flag>0
+    false
+  else
+    true
+  end
+end
+end
